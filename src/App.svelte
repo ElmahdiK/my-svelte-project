87e4cfa5ac555b2d86a10c4cfed0cddebf707c53
ElmahdiK <elmahdi.korfed@gmail.com>
2024-08-11 21:28:30 +0200

    feat: svelte-catdog added

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,101 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
+  import Button from "./lib/Button.svelte";
+
+  let imageSrc;
+  let hasError = false;
+  let isFetching = false;
+
+  $: fetchDogImage(false);
+
+  const fetchDogImage = (breed) => {
+    hasError = false;
+    imageSrc = null;
+    isFetching = true;
+    fetch(
+      breed
+        ? `https://dog.ceo/api/breed/african/images/random` // breeds/image/random
+        : `https://api.thecatapi.com/v1/images/search`
+    )
+      .then((response) => response.json())
+      .then((obj) => {
+        imageSrc = obj.message || obj[0].url;
+      })
+      .catch((error) => {
+        hasError = true;
+      })
+      .finally(() => {
+        isFetching = false;
+      });
+  };
+
+  const handleClick = (bool) => {
+    fetchDogImage(bool);
+  };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <aside>
+    <h1>Svelte Animals</h1>
+    <p>
+      Click on
+      <Button
+        on:click={(event) => {
+          event.preventDefault();
+          handleClick(false);
+        }}>cat</Button
+      > or
+      <Button
+        type="secondary"
+        on:click={(event) => {
+          event.preventDefault();
+          handleClick(true);
+        }}>dog</Button
+      >
+      to see a random image
+    </p>
+  </aside>
+  <section>
+    {#if hasError}
+      Oops. something's wrong.
+    {:else if isFetching}
+      loading...
+    {:else}
+      <img src={imageSrc} alt="Dog" />
+    {/if}
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  main {
+    display: flex;
+    height: 100dvh;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  aside {
+    background-color: #fff;
+    box-shadow: 0px 2px 4px #ccc;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+    z-index: 2;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  p {
+    margin-top: 20px;
   }
-  .read-the-docs {
-    color: #888;
+  section {
+    background-color: #fafafa;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 2;
+  }
+  img {
+    box-shadow: 1px 2px 10px rgba(0, 0, 0, 0.4);
+    height: 400px;
   }
 </style>
